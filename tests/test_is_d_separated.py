0867c523ef67161d34986edd22fbbec99a27d91e
Haris Owais <48769383+AmadeusOnBongos@users.noreply.github.com>
2025-10-31 17:00:34 +0100

    Converted more tests. Added inquire file to track which tests still need to be converted.

--- tests/test_is_d_separated.py
+++ tests/test_is_d_separated.py
@@ -1,27 +1,35 @@
 from codebase import mixed_graph as mixed
-
+import json
+import pytest
 from ..tests import Graphs_for_testing as G_testing
 
+@pytest.fixture
+def test_graphs():
+    graphs = G_testing.generate_graphs()
+    return {
+        'G1': graphs['chain'],
+        'G2': graphs['simple_collider']
+    }
 
-test_graphs = G_testing.generate_graphs()
 
-G1 = test_graphs['chain']
-G2 = test_graphs['simple_collider']
+# Load ground truth values
+def load_ground_truth():
+    with open('tests/test_answers/test_is_d_separated_answers.json', 'r') as f:
+        return json.load(f)
 
-print('G1')
-print(G1)
-print('check {X1} {X5} ')
-print(G1.is_d_separated(x={'X1',},y={'X5',},z=set(),DAG_check=False))
-print('check {X1} {X5} , {X4}')
-print(G1.is_d_separated(x={'X1',},y={'X5',},z={'X4'},DAG_check=True))
-print('check {X1} {X5} , {X4,X3}')
-print(G1.is_d_separated(x={'X1',},y={'X5',},z={'X4','X3'},DAG_check=True))
+ground_truth = load_ground_truth()
 
-print('G2')
-print(G2)
-print('check {X1} {X5} ')
-print(G2.is_d_separated(x={'X1',},y={'X5',},z=set(),DAG_check=False))
-print('check {X1} {X5} , {X4}')
-print(G2.is_d_separated(x={'X1',},y={'X5',},z={'X4'},DAG_check=True))
-print('check {X1} {X5} , {X4,X3}')
-print(G2.is_d_separated(x={'X1',},y={'X5',},z={'X4','X3'},DAG_check=True))
+
+
+def test_G1_is_d_separated(test_graphs):
+    G1 = test_graphs['G1']
+    assert G1.is_d_separated(x={'X1',},y={'X5',},z=set(),DAG_check=False) == ground_truth["G1.is_d_separated(x={'X1',},y={'X5',},z=set(),DAG_check=False)"]
+    assert G1.is_d_separated(x={'X1',},y={'X5',},z={'X4'},DAG_check=True) == ground_truth["G1.is_d_separated(x={'X1',},y={'X5',},z={'X4'},DAG_check=True)"]
+    assert G1.is_d_separated(x={'X1',},y={'X5',},z={'X4','X3'},DAG_check=True) == ground_truth["G1.is_d_separated(x={'X1',},y={'X5',},z={'X4','X3'},DAG_check=True)"]
+
+
+def test_G2_is_d_separated(test_graphs):
+    G2 = test_graphs['G2']
+    assert G2.is_d_separated(x={'X1',},y={'X5',},z=set(),DAG_check=False) == ground_truth["G2.is_d_separated(x={'X1',},y={'X5',},z=set(),DAG_check=False)"]
+    assert G2.is_d_separated(x={'X1',},y={'X5',},z={'X4'},DAG_check=True) == ground_truth["G2.is_d_separated(x={'X1',},y={'X5',},z={'X4'},DAG_check=True)"]
+    assert G2.is_d_separated(x={'X1',},y={'X5',},z={'X4','X3'},DAG_check=True) == ground_truth["G2.is_d_separated(x={'X1',},y={'X5',},z={'X4','X3'},DAG_check=True)"]
